ef04014366dfafea5d93aaa73b54e5b7f92c7903
Sheetal Kadu <sheetalkadu58@gmail.com>
2024-10-30 19:03:23 +0530

    Two-Sum.java

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,12 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int itr1 = 0; itr1 < nums.length-1; itr1++) {
-            for(int itr2 = itr1 + 1; itr2 < nums.length; itr2++) {
-                if(nums[itr1] + nums[itr2] == target) {
-                    return new int[]{itr1, itr2};
-                }
+        int[] nums1 = new int[]{0,1};
+        for(int i = 0; i < nums.length-1; i++) {
+            for(int j=i+1; j < nums.length; j++) {
+            int sum = nums[i] + nums[j];
+            if( sum == target) {
+                nums1[0] = i;
+                nums1[1] = j;
+               break;
+            } 
             }
         }
-        return null;
+        
+        return nums1;
     }
 }
